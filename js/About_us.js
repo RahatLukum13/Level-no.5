bd050b75192abbde9bfd3d1972b50cdc6bc11926
RahatLukum <karmazin2007@mail.ru>
2025-08-30 07:23:50 +0300

    update_low_js

--- js/About_us.js
+++ js/About_us.js
@@ -7,31 +7,23 @@ const sofa = document.getElementById("ab_us_sofa");
 const H = document.getElementById("ab_us_h");
 const pre = document.getElementById("ab_us_pre");
 const ul = document.getElementsByClassName("ab_us_ul")[0];
-const menuIcon = document.querySelector(`.menu-icon`);
+const menuIcon = document.querySelector(`.menu-icon`);  
 const foot = document.querySelector('.footer_ul');
 const footer1 = document.getElementById("footer");
 function toggleMenuIcon() {
     menuIcon.classList.toggle('active');
     foot.classList.toggle('active');
-    const foot_active = document.querySelector('.footer_ul.active');
-    foot.style.height = '0px';
-    foot_active.style.height = `${main.clientWidth * 0.15 + 6 * (WWidth2 / 95) / (window.devicePixelRatio * 2 / 3)}px`;
 }
 sofa.style.bottom = `-${Math.max(0, (WWidth / 3840 * 1914 - WHeight) / (window.devicePixelRatio * 2 / 3))}px`;
 sofa.style.width = `${WWidth / (window.devicePixelRatio * 2 / 3)}px`;
-H.style.fontSize = `${((WWidth / 33) - (WWidth - window.innerWidth * (window.devicePixelRatio * 2 / 3)) * 0.01) / (window.devicePixelRatio * 2 / 3)}px`;
-pre.style.fontSize = `${((WWidth / 120) - (WWidth - window.innerWidth * (window.devicePixelRatio * 2 / 3)) * 0.005) / (window.devicePixelRatio * 2 / 3)}px`;
 main.style.height = `${Math.max(WHeight / (window.devicePixelRatio * 2 / 3), ul.scrollHeight + sofa.scrollHeight * 0.5)}px`;
 menuIcon.addEventListener('click', toggleMenuIcon);
 window.addEventListener('resize', () => {
     sofa.style.bottom = `-${Math.max(0, (WWidth / 3840 * 1482 - WHeight) / (window.devicePixelRatio * 2 / 3))}px`;
     sofa.style.width = `${WWidth / (window.devicePixelRatio * 2 / 3)}px`;
-    H.style.fontSize = `${((WWidth / 33) - (WWidth - window.innerWidth * (window.devicePixelRatio * 2 / 3)) * 0.01) / (window.devicePixelRatio * 2 / 3)}px`;
-    pre.style.fontSize = `${((WWidth / 120) - (WWidth - window.innerWidth * (window.devicePixelRatio * 2 / 3)) * 0.005) / (window.devicePixelRatio * 2 / 3)}px`;
     main.style.height = `${Math.max(WHeight / (window.devicePixelRatio * 2 / 3), ul.scrollHeight + sofa.scrollHeight * 0.5)}px`;
     const foot_active = document.querySelector('.footer_ul.active');
-    foot_active.style.height = `${main.clientWidth * 0.15 + 6 * (WWidth2 / 95) / (window.devicePixelRatio * 2 / 3)}px`;
-    if(window.innerWidth > 800){
+    if(window.innerWidth > 1300){
         menuIcon.classList.toggle('active');
         foot_active.style.height = 'auto';
         foot_active.classList.toggle('active');
