570ab6fdf99929e93a7150deaf09b4a0c04c85cc
RahatLukum <karmazin2007@mail.ru>
2025-09-02 07:50:39 +0300

    dasd

--- js/contacts.js
+++ js/contacts.js
@@ -2,7 +2,9 @@ var ratio = window.screen.availWidth / window.screen.availHeight;
 const WHeight = window.screen.availHeight - (window.outerHeight - window.innerHeight * (window.devicePixelRatio * 2 / 3));
 const WWidth = window.screen.availWidth - (window.outerWidth - window.innerWidth * (window.devicePixelRatio * 2 / 3));
 ratio = WWidth / WHeight;
+const main = document.getElementById("contact");
 const img = document.getElementById("cont_img");
+main.style.height = `${WHeight}px`;
 function low(){
     img.style.opacity = "40%";
 }
