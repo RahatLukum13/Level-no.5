ecfa0066a272674e4db3b4be2cc2f90552a04865
RahatLukum <karmazin2007@mail.ru>
2025-09-13 23:31:57 +0300

    1.0.7

--- js/all.js
+++ js/all.js
@@ -1,8 +1,6 @@
-var WWidth = (window.screen.availWidth - (window.outerWidth - document.body.clientWidth * (window.devicePixelRatio * 2 / 3))) / (window.devicePixelRatio * 2 / 3);
 var WHeight = (window.screen.availHeight - (window.outerHeight - window.innerHeight * (window.devicePixelRatio * 2 / 3))) / (window.devicePixelRatio * 2 / 3);
 
 const Icon = document.querySelector(`.to-up`);
-const body = document.querySelector(`body`);
 const header_ul = document.getElementsByClassName('header_contact')[0];
 const button = document.getElementById('head_but');
 
@@ -24,12 +22,9 @@ else {
     Icon.style.opacity = "0%";
 }
 
-body.style.width = `${WWidth}px`;
 
 window.addEventListener('resize', () => {
     WHeight = (window.screen.availHeight - (window.outerHeight - window.innerHeight * (window.devicePixelRatio * 2 / 3))) / (window.devicePixelRatio * 2 / 3);
-    WWidth = (window.screen.availWidth - (window.outerWidth - document.body.clientWidth * (window.devicePixelRatio * 2 / 3))) / (window.devicePixelRatio * 2 / 3);
-    body.style.width = `${WWidth}px`;
 });
 window.addEventListener("scroll", function () {
     if (window.pageYOffset > WHeight) {
